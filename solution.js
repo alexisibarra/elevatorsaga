a78dca09170cfe76ee08a04434c7a5eab411692b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-10-14 23:10:15 -0400

    Working the solution till challenge #5

--- solution.js
+++ solution.js
@@ -1,20 +1,61 @@
 {
     init: function(elevators, floors) {
-        var elevator1 = elevators[0]; // Let's use the first elevator
-        var elevator2 = elevators[1]; // Let's use the first elevator
-
-        for (var i = 0; i < elevators.length; i++) {
-            elevators[i].on("idle", function() {
-                elevators[i].on("floor_button_pressed", function(floorNum) {
-                    elevators[i].goToFloor(floorNum);
-                })
-            });
-            elevators[i].on("floor_button_pressed", function(floorNum) {
-                elevators[i].goToFloor(floorNum);
-            })
-        }
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
-}
+            let floorsPressed = {
+                        up: [],
+                        down: [],
+                    }
+    
+            floors.forEach( floor => {
+                        floor.on("up_button_pressed", function() {
+                                        floorsPressed.up.push(floor.floorNum());
+                                    })
+                        floor.on("down_button_pressed", function() {
+                                        floorsPressed.down.push(floor.floorNum());
+                                    })
+                    })
+    
+    
+            elevators.forEach( elevator => {
+            
+                        elevator.on("stopped_at_floor", function(floorNum) {
+                                        elevator.checkDestinationQueue();                     
+                                    })
+            
+                        elevator.on("idle", () => {
+                                        console.log("idle")
+                                        if(elevator.getPressedFloors().length > 0) {
+                                                            elevator.checkDestinationQueue();                     
+                                                        }
+                        
+                                        if(elevator.destinationQueue.length === 0){
+                                                            elevator.destinationQueue = floorsPressed.up || floorsPressed.down;
+                                                            elevator.checkDestinationQueue();                     
+                                                        }
+                                    });
+                        elevator.on("passing_floor", function(floorNum, direction) {
+                                        if(floorsPressed[direction].indexOf(floorNum) >= 0){
+                                                            elevator.goToFloor(floorNum, true)
+                                                        }
+                                    });
+            
+                        elevator.on("stopped_at_floor", function(floorNum) {
+                                        ['up', 'down'].forEach( direction => {
+                                                            let index = floorsPressed[direction].indexOf(floorNum);
+                                                            if (index > -1) {
+                                                                                    floorsPressed[direction].splice(index, 1);
+                                                                                }
+                                                        })
+                                    })
+            
+                        elevator.on("floor_button_pressed", function(floorNum) {
+                                        elevator.destinationQueue.push(floorNum);            
+                                        elevator.destinationQueue.sort();
+                                        elevator.checkDestinationQueue();
+                                    })
+                    })
+    
+        },
+        update: function(dt, elevators, floors) {
+                    // We normally don't need to do anything here
+                    //         }
+                    //         }}}
